b5a2d5dd7e6b947b3397614acdad2b20994484ea
Demon Sanju <rtusharpaul@gmail.com>
2025-02-15 16:11:09 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'DEMON-XD',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "DEMON-XD",
   botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  ownerNumber: '918617018319', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
